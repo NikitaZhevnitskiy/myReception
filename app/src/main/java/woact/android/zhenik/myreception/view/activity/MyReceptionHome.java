3ddf14a75a1d873ba76cbe571017b96a71886a21
Nikita Zhevnitskiy <zhenik15@student.westerdals.no>
2017-04-29 21:59:35 +0200

    refactor my reception home class

--- app/src/main/java/woact/android/zhenik/myreception/view/activity/MyReceptionHome.java
+++ app/src/main/java/woact/android/zhenik/myreception/view/activity/MyReceptionHome.java
@@ -1,50 +1,39 @@
 package woact.android.zhenik.myreception.view.activity;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
+import android.view.View;
 
-import woact.android.zhenik.myreception.view.fragment.HotelFragment;
+import woact.android.zhenik.myreception.datalayer.entities.Hotel;
+import woact.android.zhenik.myreception.utils.ReceptionAppContext;
 import woact.android.zhenik.myreception.R;
+import woact.android.zhenik.myreception.view.listener.BottomNavigationListener;
 
 public class MyReceptionHome extends AppCompatActivity {
 
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    FragmentManager fm = getSupportFragmentManager();
-                    FragmentTransaction transaction = fm.beginTransaction();
-                    transaction.replace(R.id.content, new HotelFragment(), HotelFragment.TAG);
-                    transaction.commit();
-                    return true;
-                case R.id.navigation_dashboard:
-
-                    return true;
-                case R.id.navigation_notifications:
-
-                    return true;
-            }
-            return false;
-        }
-
-    };
+    private Hotel hotel;
+    private BottomNavigationView mNavigation;
+    private BottomNavigationListener mBottomNavListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_reception_home);
-
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        initHotel();
+        initBottomNavigation();
     }
+
+    private void initBottomNavigation() {
+        mNavigation = (BottomNavigationView) findViewById(R.id.navigation);
+        mBottomNavListener=new BottomNavigationListener(getSupportFragmentManager());
+        mNavigation.setOnNavigationItemSelectedListener(mBottomNavListener);
+        mNavigation.performClick();
+        clickHome();
+    }
+
+    private void initHotel(){hotel = (Hotel)getIntent().getSerializableExtra(ReceptionAppContext.HOTEL);}
+
+    private void clickHome(){((View)findViewById(R.id.navigation_home)).performClick();}
 
 }
