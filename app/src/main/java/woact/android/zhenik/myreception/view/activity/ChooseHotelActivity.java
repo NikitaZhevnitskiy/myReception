3eab0e7c5c55ead72ff8efe264922efae7013128
Nikita Zhevnitskiy <zhenik15@student.westerdals.no>
2017-04-29 21:54:07 +0200

    update logic and design for choose hotel activity

--- app/src/main/java/woact/android/zhenik/myreception/view/activity/ChooseHotelActivity.java
+++ app/src/main/java/woact/android/zhenik/myreception/view/activity/ChooseHotelActivity.java
@@ -1,42 +1,79 @@
 package woact.android.zhenik.myreception.view.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+
+import com.pnikosis.materialishprogress.ProgressWheel;
 
 import java.util.List;
 
 import woact.android.zhenik.myreception.R;
-import woact.android.zhenik.myreception.datalayer.DatabaseHelper;
-import woact.android.zhenik.myreception.datalayer.DatabaseManager;
 import woact.android.zhenik.myreception.datalayer.dao.CacheDao;
-import woact.android.zhenik.myreception.datalayer.dao.HotelDao;
 import woact.android.zhenik.myreception.datalayer.entities.Hotel;
+import woact.android.zhenik.myreception.utils.ApplicationContex;
 import woact.android.zhenik.myreception.view.adapter.HotelAdapter;
 
 public class ChooseHotelActivity extends AppCompatActivity {
     private static final String TAG = "CH-activity:> ";
-    private HotelAdapter hotelAdapter;
-    private ListView hotelsList;
+    private HotelAdapter mHotelAdapter;
+    private ListView mHotelsList;
+    private ProgressWheel mProgressWheel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_hotel);
-//        DatabaseManager.initializeInstance(DatabaseHelper.getHelper(getApplicationContext()));
+        mProgressWheel = (ProgressWheel) findViewById(R.id.choose_hotel_progress_wheel);
         initAdapter();
     }
 
-    private void initAdapter(){
-        hotelsList = (ListView)findViewById(R.id.choose_hotel_list);
+    @Override
+    protected void onResume() {
+        initWheel();
+        super.onResume();
+    }
+
+    private void initAdapter() {
+        mHotelsList = (ListView) findViewById(R.id.choose_hotel_list);
         List<Hotel> hotels = CacheDao.getInstance().getHotelDao().getHotels();
-//        List<Hotel> hotels = (new HotelDao()).getHotels();
-        hotelAdapter = new HotelAdapter(this, R.layout.hotel_item, hotels);
-        Log.d(TAG, hotels.size()+"");
-        if (hotelsList!=null)
-            hotelsList.setAdapter(hotelAdapter);
+        mHotelAdapter = new HotelAdapter(this, R.layout.item_list_hotel, hotels);
+        Log.d(TAG, hotels.size() + "");
+        if (mHotelsList != null)
+            mHotelsList.setAdapter(mHotelAdapter);
+        initListViewListener();
+    }
+
+    private void initWheel() {
+        if (mProgressWheel.isSpinning()) {
+            mProgressWheel.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void initListViewListener() {
+        mHotelsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Hotel hotel = ((Hotel) mHotelsList.getItemAtPosition(position));
+                final Intent intent = new Intent(getApplicationContext(), MyReceptionHome.class);
+                intent.putExtra(ApplicationContex.HOTEL, hotel);
+                mProgressWheel.setVisibility(View.VISIBLE);
+
+                // emulate download database to cache
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    public void run() {
+
+                        startActivity(intent);
+                    }
+                }, 1500);
+            }
+        });
     }
 
 
