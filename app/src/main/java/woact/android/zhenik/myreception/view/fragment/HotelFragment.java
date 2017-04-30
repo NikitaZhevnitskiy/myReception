4d5f406fa45398e1a03b16b5f11038d70533067d
Nikita Zhevnitskiy <zhenik15@student.westerdals.no>
2017-04-30 18:58:06 +0200

    update hotel fragment

--- app/src/main/java/woact/android/zhenik/myreception/view/fragment/HotelFragment.java
+++ app/src/main/java/woact/android/zhenik/myreception/view/fragment/HotelFragment.java
@@ -1,5 +1,6 @@
 package woact.android.zhenik.myreception.view.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,12 +8,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import woact.android.zhenik.myreception.R;
+import woact.android.zhenik.myreception.view.activity.information.InformationActivity;
+import woact.android.zhenik.myreception.view.listener.ScreenNavigationListener;
 
 
 public class HotelFragment extends Fragment {
-    public static final String TAG = "HotelFragment";
+
+    public static final String TAG = "HotelFragment:> ";
     private View view;
 
     public HotelFragment() {}
@@ -23,7 +28,23 @@ public class HotelFragment extends Fragment {
         Log.i(TAG, "onCreateView");
         setHasOptionsMenu(true);
         this.view=inflater.inflate(R.layout.fragment_hotel, null);
+        initNavigationListeners();
         return view;
+    }
+    private void initNavigationListeners(){
+
+        ScreenNavigationListener screenNavigationListener = new ScreenNavigationListener(getContext());
+        ((LinearLayout)view.findViewById(R.id.screen_navigation_information))
+                .setOnClickListener(screenNavigationListener);
+//                    @Override
+//                    public void onClick(View v) {
+//                        if (v.getId()==R.id.screen_navigation_information){
+//                            startActivity(new Intent(getContext(), InformationActivity.class));
+//                        }
+//                    }
+//                });
+
+
     }
 
 }
