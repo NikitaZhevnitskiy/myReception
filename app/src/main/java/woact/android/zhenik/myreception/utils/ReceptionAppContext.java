509dc305b38b388431e682b6a9db592dbacb0809
Nikita Zhevnitskiy <zhenik15@student.westerdals.no>
2017-05-01 00:42:31 +0200

    update reception app context

--- app/src/main/java/woact/android/zhenik/myreception/utils/ReceptionAppContext.java
+++ app/src/main/java/woact/android/zhenik/myreception/utils/ReceptionAppContext.java
@@ -1,5 +1,15 @@
 package woact.android.zhenik.myreception.utils;
 
+import woact.android.zhenik.myreception.datalayer.entities.Hotel;
+
 public class ReceptionAppContext {
-    public static final String HOTEL = "HOTEL";
+    public static final String HOTEL_IN_SYSTEM = "HOTEL_IN_SYSTEM";
+
+    public static Hotel hotel = null;
+    public static Hotel getHotel() {return hotel;}
+    public static void setHotel(Hotel hotel) {ReceptionAppContext.hotel = hotel;}
+
+    public static Long hotelId=null;
+    public static Long getHotelId() {return hotelId;}
+    public static void setHotelId(Long hotelId) {ReceptionAppContext.hotelId = hotelId;}
 }
