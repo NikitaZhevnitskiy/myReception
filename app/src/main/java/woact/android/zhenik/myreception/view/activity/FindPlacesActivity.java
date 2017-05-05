61e68c749e59fc14941dee98902bb2e03528c776
Nikita Zhevnitskiy <zhenik15@student.westerdals.no>
2017-05-05 21:19:20 +0200

    add marker feature to find places

--- app/src/main/java/woact/android/zhenik/myreception/view/activity/FindPlacesActivity.java
+++ app/src/main/java/woact/android/zhenik/myreception/view/activity/FindPlacesActivity.java
@@ -4,26 +4,38 @@ import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import woact.android.zhenik.myreception.R;
+import woact.android.zhenik.myreception.datalayer.dao.CacheDao;
+import woact.android.zhenik.myreception.datalayer.entities.Hotel;
+import woact.android.zhenik.myreception.utils.ReceptionAppContext;
 
-public class FindPlacesActivity extends AppCompatActivity {
+public class FindPlacesActivity extends FragmentActivity implements OnMapReadyCallback {
 
 
     public static final String TAG = "FindPlacesActivity:> ";
@@ -32,13 +44,21 @@ public class FindPlacesActivity extends AppCompatActivity {
      * Request code passed to the PlacePicker intent to identify its result when it returns.
      */
     private static final int PLACE_PICKER_REQUEST = 1;
+    private GoogleMap map;
+    private Hotel hotel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_places);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setTitle("Find near by");
+        setActionBar(new Toolbar(this));
+//        getActionBar().setDisplayHomeAsUpEnabled(true);
+//        getActionBar().setTitle("Find near by");
+        //Handle when activity is recreated like on orientation Change
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.find_places_map);
+        mapFragment.getMapAsync(this);
+       hotel = CacheDao.getInstance().getHotelDao().getHotel(ReceptionAppContext.getHotelId());
         initFindButton();
     }
     // Back btn in action bar
@@ -65,11 +85,11 @@ public class FindPlacesActivity extends AppCompatActivity {
         List<Address> addresses=new ArrayList<>();
         PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
         try {
-            addresses = geocoder.getFromLocationName("Chr. Krohgs gate 32, 0186 Oslo", 1);
+            addresses = geocoder.getFromLocationName(hotel.getAddress(), 1);
             if(addresses.size() > 0) {
                 double latitude= addresses.get(0).getLatitude();
                 double longitude= addresses.get(0).getLongitude();
-                LatLng westerdals = new LatLng(latitude, longitude);
+
                 LatLngBounds bounds =
                         new LatLngBounds(
                                 new LatLng(latitude-0.0015, longitude-0.0015),
@@ -94,10 +114,53 @@ public class FindPlacesActivity extends AppCompatActivity {
                 // do smth with place - make a route f.example
                 String toastMsg = String.format("Place: %s", place.toString());
                 Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
+                addMarker(place);
 //                initCardPlace(place);
             }
         }
     }
+
+    private void addMarker(Place place) {
+        LatLng placeLatLng = place.getLatLng();
+        map.addMarker(new MarkerOptions().position(placeLatLng).title(place.getName()+""));
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map=googleMap;
+//        googleMap.addMarker(new MarkerOptions()
+//                              .position(new LatLng(0, 0))
+//                              .title("Marker"));
+
+        Geocoder geocoder = new Geocoder(getApplicationContext());
+        List<Address> addresses=new ArrayList<>();
+        try {
+//            addresses = geocoder.getFromLocationName("Chr. Krohgs gate 32, 0186 Oslo", 1);
+            addresses = geocoder.getFromLocationName(hotel.getAddress(), 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if(addresses.size() > 0) {
+            double latitude= addresses.get(0).getLatitude();
+            double longitude= addresses.get(0).getLongitude();
+            LatLng place = new LatLng(latitude, longitude);
+            map.addMarker(new MarkerOptions().position(place).title(hotel.getName()));
+//            map.moveCamera(CameraUpdateFactory.newLatLng(westerdals));
+
+
+            CameraPosition googlePlex = CameraPosition.builder()
+                    .target(place)
+                    .zoom(16)
+                    .bearing(0)
+                    .tilt(45)
+                    .build();
+
+            map.moveCamera(CameraUpdateFactory.newCameraPosition(googlePlex));
+        }
+    }
+
+
 //
 //    private void displayPlace( Place place ) {
 //        if( place == null )
