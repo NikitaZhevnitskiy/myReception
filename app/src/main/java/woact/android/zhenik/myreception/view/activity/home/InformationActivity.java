0ed4869316c422c992e727434be240e9b7fc3048
Nikita Zhevnitskiy <zhenik15@student.westerdals.no>
2017-05-05 20:40:31 +0200

    test asyncTask with linearLayout

--- app/src/main/java/woact/android/zhenik/myreception/view/activity/home/InformationActivity.java
+++ app/src/main/java/woact/android/zhenik/myreception/view/activity/home/InformationActivity.java
@@ -1,9 +1,19 @@
 package woact.android.zhenik.myreception.view.activity.home;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
 
 import woact.android.zhenik.myreception.R;
 import woact.android.zhenik.myreception.datalayer.dao.CacheDao;
@@ -17,11 +27,14 @@ public class InformationActivity extends AppCompatActivity {
     private TextView hotelTelephone;
     private TextView hotelEmail;
     private Hotel currentHotel;
+    private LinearLayout layoutPic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
+        layoutPic = (LinearLayout)findViewById(R.id.information_activity_pic);
+        layoutPic.setBackground(getResources().getDrawable(R.drawable.info_description1));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("INFORMATION");
 
@@ -55,6 +68,36 @@ public class InformationActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         onBackPressed();
         return true;
+    }
+
+    class MyTask extends AsyncTask<Drawable, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+//            tvInfo.setText("Begin");
+        }
+
+        @Override
+        protected Void doInBackground(Drawable... params) {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+//                byte[] decodedString = Base64.decode(getResources().getDrawable(R.drawable.info_description1, ), Base64.NO_WRAP);
+//                InputStream inputStream  = new ByteArrayInputStream(decodedString);
+//                Bitmap bitmap  = BitmapFactory.decodeStream(inputStream);
+//                layoutPic.setBackground(params[0]);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+//            tvInfo.setText("End");
+        }
     }
 
 
