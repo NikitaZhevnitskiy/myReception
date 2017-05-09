5c1ecf843cb06eca9d3e62871bb08ddb6b2ea2f4
Nikita Zhevnitskiy <zhenik15@student.westerdals.no>
2017-05-09 20:12:49 +0200

    update choose hotel activity

--- app/src/main/java/woact/android/zhenik/myreception/view/activity/ChooseHotelActivity.java
+++ app/src/main/java/woact/android/zhenik/myreception/view/activity/ChooseHotelActivity.java
@@ -35,6 +35,7 @@ public class ChooseHotelActivity extends AppCompatActivity {
         setContentView(R.layout.activity_choose_hotel);
         mProgressWheel = (ProgressWheel) findViewById(R.id.choose_hotel_progress_wheel);
         mTxtSearch = (EditText)findViewById(R.id.choose_hotel_search);
+        getSupportActionBar().setTitle("Choose where you stay");
 
 //        initAdapter();
     }
